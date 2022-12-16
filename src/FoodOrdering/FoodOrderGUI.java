f2a4bbe0b4e9b9a94d036d83e512e3d1ccb45797
EternalNewbiee <secuya.francis.michael@gmail.com>
2022-12-17 01:22:28 +0800

    Complete FoodOrder

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,6 +2,8 @@ package FoodOrdering;
 
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -25,5 +27,31 @@ public class FoodOrderGUI extends JFrame{
         app.setTitle("Food Ordering System");
         app.setVisible(true);
     }
+    FoodOrderGUI(){
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+//                try {
+                    double total = 0;
+                    if (cPizza.isSelected()) total += 100;
+                    if (cBurger.isSelected()) total += 80;
+                    if (cFries.isSelected()) total += 65;
+                    if (cSoftDrinks.isSelected()) total += 55;
+                    if (cTea.isSelected()) total += 50;
+                    if (cSundae.isSelected()) total += 40;
 
+                    if (rb5.isSelected()) total -= total * .05;
+                    if (rb10.isSelected()) total -= total * .10;
+                    if (rb15.isSelected()) total -= total * .15;
+
+//                    if(total==0) throw(new NoOrderException());
+                    JOptionPane.showMessageDialog(null, "The total price is Php " + String.format("%.2f", total));
+//                }catch (NoOrderException ex) {
+//                    JOptionPane.showMessageDialog(null,"Please Select a Food Item","INVALID ORDER", JOptionPane.ERROR_MESSAGE);
+//                }
+            }
+        });
+
+    }
 }
+
